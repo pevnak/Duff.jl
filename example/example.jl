2dab49ffa88899fd3c7c0fb74c333cccf40e1af2
Račinský Matěj <matej.racinsky@avast.com>
2020-01-21 10:22:08 +0100

    some changes, updated dependencies, added travis file

--- example/example.jl
+++ example/example.jl
@@ -1,6 +1,6 @@
 using Revise
 using Duff, Flux, MLDataPattern, DataFrames, FileIO, ArgParse, Statistics
-using Duff: onlinedaf
+using Duff: onlinedaf, meanscore
 
 s = ArgParseSettings()
 s.description = ""
@@ -19,54 +19,24 @@ settings = parse_args(ARGS, s; as_symbols=true)
 #			At first, we create a function which performs one iteration of the algorithms
 #############################################################################
 zeroitems!(x, mask::Vector{Int}) = x[setdiff(1:size(x,1),mask),:] .= 0
-function onestep(mask, model, opt, dataprovider)
+function onestep(mask, model, opt, dataprovider, ps)
 	x, y = getobs.(dataprovider())
 	zeroitems!(x, mask)
 	f = Flux.crossentropy(max.(softmax(model(x))),Flux.onehotbatch(y,1:2))
-	Flux.Tracker.back!(f)
-	opt()
-	isnan(Flux.data(f)) && error("nan in the model")
-	Flux.data(f)
+	Flux.Optimise.update!(opt, ps, gradient(() -> f, ps))
+	isnan(f) && error("nan in the model")
+	f
 end
 
-loaddataset(f) = (load(f,"data"),load(f,"labels"))
-X,Y =  loaddataset("/Users/tpevny/Work/Data/featureselection/madelon_train_nips_500.jld2")
+X = readlines("example/madelon_train.data") .|> (x->(x |> split .|> x->parse(Int, x))) |> x->hcat(x...)
+Y = readlines("example/madelon_train.labels") .|> x->parse(Int, x)
+Y[Y.==-1] .= 2
 dim = size(X,1)
 p, nprobes, bs = 0.1, Int(1e5), 100
 trndata,tstdata = splitobs(shuffleobs((X,Y)), 0.5)
 model = Flux.Chain(Dense(dim,settings[:k],relu), [Dense(settings[:k],settings[:k],relu) for _ in 1:settings[:l]]..., Dense(settings[:k],2))
-opt = Flux.ADAM(params(model))
-daf = onlinedaf(dim, mask -> onestep(mask, model, opt, () -> randobs(trndata,bs)), p, nprobes)
+opt = Flux.ADAM()
+ps = params(model)
+daf = onlinedaf(dim, mask -> onestep(mask, model, opt, () -> randobs(trndata,bs), ps), p, nprobes)
 
-function trainmodel!(model,idx,data)
-	opt = Flux.ADAM(params(model))
-	for x in data
-		xx = getobs(x)
-		onestep(model,opt,xx[1][idx,:],xx[2])
-	end
-end
-
-evaluatemodel(m,idx,x,y) = mean(Flux.argmax(softmax(m(x[idx,:]))) .!= y)
-function evaluatefmodel(m,idx,x,y) 
-	x[setdiff(1:size(x,1),idx),:] .= 0
-	mean(Flux.argmax(softmax(m(x))) .!= y)
-end
-
-#############################################################################
-#		Evaluate the model by training the model on subset of features
-#############################################################################
-score = mscore(dafstats)
-I = sortperm(score,rev=true)
-data = RandomBatches(trndata,settings[:batchsize],settings[:eiterations])
-df = [];
-for i in 1:length(I)
-	idx = I[1:i]
-	model = Flux.Chain(Dense(i,dim,relu),Dense(dim,dim,relu),Dense(dim,4,relu),Dense(4,2))
-	trainmodel!(model,idx,data)
-	e = evaluatemodel(model,idx,getobs(tstdata)...)
-	ef = evaluatefmodel(fmodel,idx,getobs(tstdata)...)
-	push!(df,DataFrame(features = i, feature = I[i], score = score[i], error = e, ferror = ef))
-end
-
-display(vcat(df...))
-
+score = meanscore(daf)
